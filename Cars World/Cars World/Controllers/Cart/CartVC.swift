eda20627e47813136a4e8fb7026d22e641a2760e
SyedJaved111 <adyyar444@gmail.com>
2018-11-26 19:24:56 +0500

    Completed the following screen designs 1- Product Detail 2- Cart 3- Cart Form 4- Payment 5- Seller Order screens

---
+++ Cars World/Cars World/Controllers/Cart/CartVC.swift
@@ -0,0 +1,30 @@
+//
+//  CartVC.swift
+//  Cars World
+//
+//  Created by Apple on 26/11/2018.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class CartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
