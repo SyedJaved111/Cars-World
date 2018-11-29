dda751dafb5f5f76258e64cc418c6c4effac484e
SyedJaved111 <adyyar444@gmail.com>
2018-11-29 18:54:46 +0500

    gdd

---
+++ Cars World/Cars World/Controllers/Product/ProductAddCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductAddCell.swift
+//  Cars World
+//
+//  Created by Apple on 29/11/2018.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ProductAddCell: UICollectionViewCell {
+    
+}
---
+++ Cars World/Cars World/Extensions/CustomImagePageControl.swift
@@ -0,0 +1,9 @@
+//
+//  CustomImagePageControl.swift
+//  Cars World
+//
+//  Created by Apple on 29/11/2018.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import Foundation
